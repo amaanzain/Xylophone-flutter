2fdf5cf2b4739f0f4b3b58d0b190902589fe2baf
zain <zainamaanaz@gmail.com>
2023-05-25 03:10:27 +0530

    Xylophone app final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSounds(int a) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$a.wav'));
+    print('button $a pressed');
+  }
+  Expanded tiles(int no,Color col){
+    return Expanded(
+      child: Container(
+        color: col,
+        child: TextButton(
+          onPressed: () {
+            playSounds(no);
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              tiles(1,Colors.red),
+              tiles(2,Colors.orange),
+              tiles(3,Colors.yellow),
+              tiles(4,Colors.green),
+              tiles(5,Colors.green.shade900),
+              tiles(6,Colors.blue),
+              tiles(7,Colors.purple),
+            ],
+          ),
         ),
       ),
     );
